7efa76c57bc92f05592711fd09d4fd2be3d52d17
Akkil MG <akkilcharanmg@gmail.com>
2025-03-13 17:19:00 +0530

    Fix

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,28 +3,27 @@ import { useEffect, useState } from "react";
 
 const Hero = ({ team }: any) => {
     const picdb = "https://picdb.avianintek.com";
-    console.log(team);
     const [mention, setMention] = useState(0);
-    const [hand, setHand] = useState("assets/images/tablet-in-hand.png");
+    // const [hand, setHand] = useState("assets/images/tablet-in-hand.png");
     useEffect(() => {
         const randomMention = Math.floor(Math.random() * team.length);
         setMention(randomMention);
     }, []);
 
-    useEffect(() => {
-        const updateHandImage = () => {
-            if (window.innerWidth > 768) {
-                setHand("assets/images/mobile-in-hand.png");
-            } else {
-                setHand("assets/images/tablet-in-hand.png");
-            }
-        };
+    // useEffect(() => {
+    //     const updateHandImage = () => {
+    //         if (window.innerWidth > 768) {
+    //             setHand("assets/images/mobile-in-hand.png");
+    //         } else {
+    //             setHand("assets/images/tablet-in-hand.png");
+    //         }
+    //     };
 
-        updateHandImage();
-        window.addEventListener("resize", updateHandImage);
+    //     updateHandImage();
+    //     window.addEventListener("resize", updateHandImage);
 
-        return () => window.removeEventListener("resize", updateHandImage); // Cleanup
-    }, []);
+    //     return () => window.removeEventListener("resize", updateHandImage); // Cleanup
+    // }, []);
 
 
     return (
@@ -55,8 +54,8 @@ const Hero = ({ team }: any) => {
             <div className="absolute top-0 right-0 h-full hidden md:block">
                 <img draggable={false} src="assets/images/background.png" alt="Background mobile in hand" className="h-full object-cover" />
             </div>
-            <div className="absolute bottom-0 right-0 mx-auto w-full max-w-xs sm:max-w-sm md:max-w-lg md:max-w-xl">
-                <img draggable={false} src={hand} alt="Mobile in Hand" width={650} height={710} className="relative z-20 drop-shadow-2xl" />
+            <div className="absolute hidden md:block bottom-0 right-0 mx-auto w-full max-w-xs sm:max-w-sm md:max-w-lg md:max-w-xl">
+                <img draggable={false} src="/assets/images/mobile-in-hand.png" alt="Mobile in Hand" width={650} height={710} className="relative z-20 drop-shadow-2xl" />
             </div>
             <div className="absolute bottom-10 right-64 transform -translate-x-1/2 hidden lg:flex flex-col space-y-8 items-center z-30">
                 <div className="flex items-center bg-blue-100 text-blue-900 rounded-lg shadow-md w-72 border-2 border-blue-500 transform -rotate-6">
